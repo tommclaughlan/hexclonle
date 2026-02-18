567406cf086650557e4f3d5b9f01047b51ae3937
Thomas McLaughlan <tmclaughlan@scottlogic.com>
2026-02-18 13:57:35 +0000

    Use hash routing rather than base-href, as github pages doesn't behave well

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import {provideRouter, withComponentInputBinding} from '@angular/router';
+import {provideRouter, withComponentInputBinding, withHashLocation} from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withComponentInputBinding())
+    provideRouter(routes, withComponentInputBinding(), withHashLocation())
   ]
 };
